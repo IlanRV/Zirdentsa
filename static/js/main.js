5f5c99cdd966dab8dc6b5cecb06f9bcf317df908
IlanRV <ilanramirez2010@hotmail.com>
2025-07-28 16:16:56 +0100

    This is bad practice but I actually reworked the sizing of sections in this branch. Now the site is fully modular on different devices and the maindash has much better spacing. Also added functionality for certain buttons as contactanos takes you to the contact form, same with the one in main dash. TODO: Fix the video to the hero banner.

--- static/js/main.js
+++ static/js/main.js
@@ -10,3 +10,28 @@ if (typeof hasBanner !== 'undefined' && hasBanner) {
 } else {
   document.querySelector('.navbar').classList.add('bg-dark');
 }
+
+
+
+
+// YT JS https://www.youtube.com/watch?v=iXlkRhjnnpk
+
+document.addEventListener("DOMContentLoaded", () => {
+      const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add('in-view');
+            return;
+          }
+          entry.target.classList.remove('in-view');
+        });
+      });
+
+      const AllAnimatedElements = document.querySelectorAll('.animate');
+      AllAnimatedElements.forEach((element) => observer.observe(element));
+});
+
+
+
+
+
